22f21688787a11a3437c0d031f1c123f40b96d8c
jsoneclick <51366467+jsoneclick@users.noreply.github.com>
2023-08-02 13:23:17 +0300

    Minor shanges

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Vladyslav Starzhinskyi
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Senior Admin
                 </Typography>
               </Box>
             </Box>
